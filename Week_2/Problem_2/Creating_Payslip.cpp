502f2057e8e74789da4135b6e2c274ab4682adc2
harfi-ugm <muhamadharfiibadurahman@mail.ugm.ac.id>
2024-09-17 05:58:46 +0000

    Something

--- Week_2/Problem_2/Creating_Payslip.cpp
+++ Week_2/Problem_2/Creating_Payslip.cpp
@@ -4,34 +4,32 @@
 using namespace std;
 int main () {
 //variable
-string name;
-double baseSalary = 15000000; // base salary
-double installment;
-double insurance;
-// Input form the user
-cout << "Enter Name: ";
-getline(cin, name);
-cout << "Enter Installment Amount (Rp): ";
-cin >> installment;
-cout << "Insurance Amount (Rp): " ;
-cin >> insurance;
-//calculate - this calculate must below input, if not the result is not
-calculate
-double tax = baseSalary * 0.2;
-double netSalary = baseSalary - installment - tax -insurance;
-// Function to display the result
-cout << "Payslip for Employee" << endl;
-cout << "---------------------" << endl;
-cout << "Name: " << name <<endl;
-cout << "Gross Salary: Rp" <<fixed << setprecision(2) << baseSalary <<
-endl;
-cout << "Tax (20%)" << tax <<endl ;
-cout << "Installment: Rp" << fixed << setprecision(2) << installment <<
-endl;
-cout << "Insurance: Rp" << fixed << setprecision(2) << insurance <<
-endl;
-cout << "Net Salary: Rp " << fixed << setprecision(2) << netSalary <<
-endl;
+    string name;
+    double baseSalary = 15000000; // base salary
+    double installment;
+    double insurance;
 
-return 0; // End of the program
+// Input form the user
+    cout << "Enter Name: ";
+    getline(cin, name);
+    cout << "Enter Installment Amount (Rp): ";
+    cin >> installment;
+    cout << "Insurance Amount (Rp): " ;
+    cin >> insurance;
+
+//calculate - this calculate must below input, if not the result is not calculate
+    double tax = baseSalary * 0.2;
+    double netSalary = baseSalary - installment - tax -insurance;
+
+// Function to display the result
+    cout << "Payslip for Employee" << endl;
+    cout << "---------------------" << endl;
+    cout << "Name: " << name <<endl;
+    cout << "Gross Salary: Rp" <<fixed << setprecision(2) << baseSalary << endl;
+    cout << "Tax (20%)" << tax <<endl ;
+cout << "Installment: Rp" << fixed << setprecision(2) << installment << endl;
+cout << "Insurance: Rp" << fixed << setprecision(2) << insurance << endl;
+cout << "Net Salary: Rp " << fixed << setprecision(2) << netSalary << endl;
+
+    return 0; // End of the program
 }
