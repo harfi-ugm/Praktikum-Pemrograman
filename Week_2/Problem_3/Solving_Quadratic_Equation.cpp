6c165cf18f12b2b496dc7bd8b7e5b24be7d4943a
harfi-ugm <muhamadharfiibadurahman@mail.ugm.ac.id>
2024-09-17 13:26:13 +0700

    Create Solving_Quadratic_Equation.cpp

---
+++ Week_2/Problem_3/Solving_Quadratic_Equation.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath> // it's for sqrt() function
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cout << "Enter coefficients a, b, and c: ";
+    cin >> a >> b >> c; //
+
+    // Calculate the discriminant
+    double discriminant = b * b - 4 * a * c;
+
+    // Check the discriminant and calculate roots accordingly
+    if (discriminant > 0) {
+        // Two distinct real roots
+        double x1 = (-b + sqrt(discriminant)) / (2 * a);
+        double x2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "The equation has two distinct real roots: " << endl;
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+        
+    } else if (discriminant == 0) {
+        // One real root
+        double x = -b / (2 * a);
+        cout << "The equation has one real root: " << std::endl;
+        cout << "x = " << x << endl;
+        
+    } else {
+        // No real roots, complex roots
+        double realPart = -b / (2 * a);
+        double imaginaryPart = sqrt(-discriminant) / (2 * a);
+        cout << "The equation has complex roots: " << endl;
+        cout << "x1 = " << realPart << " + " << imaginaryPart << "i" << endl;
+        cout << "x2 = " << realPart << " - " << imaginaryPart << "i" << endl;
+    }
+
+    return 0;
+}
