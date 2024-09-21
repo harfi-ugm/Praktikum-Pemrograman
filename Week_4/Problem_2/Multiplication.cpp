e23b7dbda3a41852473a4e0dfbd9fb06cf3db2bc
harfi-ugm <muhamadharfiibadurahman@mail.ugm.ac.id>
2024-09-21 03:40:15 +0000

    update

--- Week_4/Problem_2/Multiplication.cpp
+++ Week_4/Problem_2/Multiplication.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// Function to print multiplication table for n
+void multiplicationTable(int n) {
+    for (int i = 1; i <= 10; ++i) {
+        cout << n << " x " << i << " = " << n * i << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: " << endl;
+    cin >> n;
+    multiplicationTable(n);
+    cout << endl;
+    return 0;
+}
 
