043ecc9f2fb8c06086df85bda975be22ee33f2f2
harfi-ugm <muhamadharfiibadurahman@mail.ugm.ac.id>
2024-11-25 14:07:46 +0700

    Create 2largestnumber.cpp

---
+++ responsi/number7/2largestnumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(){
+    vector<int> nums = {1,2,3,4,5};
+    
+    
+    
+    
+    cout << "Second largest number is: " << nums[3] << endl;
+}
