dbfb67f870e0b323ee68d7008a232452fb329b86
harfi-ugm <muhamadharfiibadurahman@mail.ugm.ac.id>
2024-11-25 14:18:50 +0700

    Update 2largestnumber.cpp

--- responsi/number7/2largestnumber.cpp
+++ responsi/number7/2largestnumber.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-
-int main(){
-    vector<int> nums = {1,2,3,4,5};
-    
-    
-    
-    
-    cout << "Second largest number is: " << nums[3] << endl;
+int main() {
+    vector<int> nums= {1,2,3,4,5};
+    sort(nums.begin(), nums.end(), greater<int>());
+    cout << "The second largest number is: " << nums[1] << endl;
+    return 0;
 }
